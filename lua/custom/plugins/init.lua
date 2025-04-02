db31bbba9552e4c623dafa343c29dae049ccae22
ben <isieldor@gmail.com>
2025-04-02 12:20:55 -0700

    Disable null-ls bc it double-formats ts, use conform instead

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -100,73 +100,76 @@ return {
       }
     end,
   },
-  {
-    'nvimtools/none-ls.nvim',
-    dependencies = {
-      'nvimtools/none-ls-extras.nvim',
-    },
-    config = function()
-      local null_ls = require 'null-ls'
-
-      local group = vim.api.nvim_create_augroup('lsp_format_on_save', { clear = false })
-      local event = 'BufWritePre' -- or "BufWritePost"
-      local async = event == 'BufWritePost'
-
-      null_ls.setup {
-        on_attach = function(client, bufnr)
-          if client.supports_method 'textDocument/formatting' then
-            vim.keymap.set('n', '<Leader>f', function()
-              vim.lsp.buf.format { bufnr = vim.api.nvim_get_current_buf() }
-            end, { buffer = bufnr, desc = '[lsp] format' })
-
-            -- format on save
-            vim.api.nvim_clear_autocmds { buffer = bufnr, group = group }
-            vim.api.nvim_create_autocmd(event, {
-              buffer = bufnr,
-              group = group,
-              callback = function()
-                vim.lsp.buf.format { bufnr = bufnr, async = async }
-              end,
-              desc = '[lsp] format on save',
-            })
-          end
-
-          if client.supports_method 'textDocument/rangeFormatting' then
-            vim.keymap.set('x', '<Leader>f', function()
-              vim.lsp.buf.format { bufnr = vim.api.nvim_get_current_buf() }
-            end, { buffer = bufnr, desc = '[lsp] format' })
-          end
-        end,
-        sources = {
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.black,
-        },
-      }
-    end,
-  },
-  {
-    'MunifTanjim/prettier.nvim',
-    init = function()
-      local prettier = require 'prettier'
-      prettier.setup {
-        bin = 'prettierd', -- or `'prettierd'` (v0.23.3+)
-        filetypes = {
-          'css',
-          'graphql',
-          'html',
-          'javascript',
-          'javascriptreact',
-          'json',
-          'less',
-          'markdown',
-          'scss',
-          'typescript',
-          'typescriptreact',
-          'yaml',
-        },
-      }
-    end,
-  },
+  --
+  -- DISABLING NULL LS BECAUSE IT WAS MAKING PRETTIER MISBEHAVE AND THAT'S BAD
+  --
+  -- {
+  --   'nvimtools/none-ls.nvim',
+  --   dependencies = {
+  --     'nvimtools/none-ls-extras.nvim',
+  --   },
+  --   config = function()
+  --     local null_ls = require 'null-ls'
+  --
+  --     local group = vim.api.nvim_create_augroup('lsp_format_on_save', { clear = false })
+  --     local event = 'BufWritePre' -- or "BufWritePost"
+  --     local async = event == 'BufWritePost'
+  --
+  --     null_ls.setup {
+  --       on_attach = function(client, bufnr)
+  --         if client.supports_method 'textDocument/formatting' then
+  --           vim.keymap.set('n', '<Leader>f', function()
+  --             vim.lsp.buf.format { bufnr = vim.api.nvim_get_current_buf() }
+  --           end, { buffer = bufnr, desc = '[lsp] format' })
+  --
+  --           -- format on save
+  --           vim.api.nvim_clear_autocmds { buffer = bufnr, group = group }
+  --           vim.api.nvim_create_autocmd(event, {
+  --             buffer = bufnr,
+  --             group = group,
+  --             callback = function()
+  --               vim.lsp.buf.format { bufnr = bufnr, async = async }
+  --             end,
+  --             desc = '[lsp] format on save',
+  --           })
+  --         end
+  --
+  --         if client.supports_method 'textDocument/rangeFormatting' then
+  --           vim.keymap.set('x', '<Leader>f', function()
+  --             vim.lsp.buf.format { bufnr = vim.api.nvim_get_current_buf() }
+  --           end, { buffer = bufnr, desc = '[lsp] format' })
+  --         end
+  --       end,
+  --       sources = {
+  --         null_ls.builtins.formatting.prettier,
+  --         null_ls.builtins.formatting.black,
+  --       },
+  --     }
+  --   end,
+  -- },
+  -- {
+  --   'MunifTanjim/prettier.nvim',
+  --   init = function()
+  --     local prettier = require 'prettier'
+  --     prettier.setup {
+  --       bin = 'prettierd', -- or `'prettierd'` (v0.23.3+)
+  --       filetypes = {
+  --         'css',
+  --         'graphql',
+  --         'html',
+  --         'javascript',
+  --         'javascriptreact',
+  --         'json',
+  --         'less',
+  --         'markdown',
+  --         'scss',
+  --         'typescript',
+  --         'typescriptreact',
+  --         'yaml',
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     'lewis6991/hover.nvim',
     init = function()
